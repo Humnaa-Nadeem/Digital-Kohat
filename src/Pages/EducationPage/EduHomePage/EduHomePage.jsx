d7e8c8f0c920c1dbefdbf9046d24c7e9184e0bc7
awaisanwarcode <muhammadayub56122@gmail.com>
2025-11-12 13:35:13 +0500

    Set up of routing system and added eduction page.

---
+++ src/Pages/EducationPage/EduHomePage/EduHomePage.jsx
@@ -0,0 +1,9 @@
+export const EduHomePage = () => {
+    return (
+        <>
+        <header>
+            
+        </header>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { EduHomePage } from './Pages/EducationPage/EduHomePage/EduHomePage';
+
+// This is the routing system.
+const allRoutes = createBrowserRouter([{
+  path: "/",
+  element: <App />
+}
+  ,
+{
+  path: "/education",
+  element: <EduHomePage />
+}])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={allRoutes} />
 );
 
 // If you want to start measuring performance in your app, pass a function
